b6e7731edd074950e1a7d765a0b0e9791a937ede
Bhavika Prakash Chavan <bhavikachavan852004@gmail.com>
2024-12-23 23:34:08 +0530

    Update reverse_string.java

--- String_questions/reverse_string.java
+++ String_questions/reverse_string.java
@@ -1,21 +1,36 @@
 package string_based_questions;
 
+// import java.util.Scanner;
 public class reverse_string {
 
     public static void main(String[] args) {
-        StringBuilder str = new StringBuilder("Hello");
 
+        //1st way
+        String str = "Hello";
+        char[] chArr = str.toCharArray();
+        for (int i = chArr.length - 1; i >= 0; i--) {
+            System.out.print(chArr[i]);
+        }
+        System.out.println();
+
+        //2nd Way
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
+        System.out.println();
+
+        //3rd way
+        StringBuffer sb = new StringBuffer(str);
+        System.out.print(sb.reverse());
+
+        StringBuilder str1 = new StringBuilder("Hello");
         for (int i = 0; i < str.length() / 2; i++) {
             int front = i;
-            int back = str.length() - i - 1;
-
-            char backchar = str.charAt(back);
-            char frontchar = str.charAt(front);
-
-            str.setCharAt(front, backchar);
-            str.setCharAt(back, frontchar);
+            int back = str1.length() - i - 1;
+            char frontchar = str1.charAt(front);
+            char backchar = str1.charAt(back);
+            str1.setCharAt(front, backchar);
+            str1.setCharAt(back, frontchar);
         }
-        System.out.println(str);
-
     }
 }
